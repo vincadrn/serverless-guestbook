9d41fc4e70de49bdfa837ecf7b9cfb932c4769e2
Vincentius Adrian <42486755+vincadrn@users.noreply.github.com>
2022-09-01 00:41:48 +0700

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '/guestbook';
+const apiUrl = 'https://7de01c5c.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
